2e128a5750f771b6893d182e18ddf2f8a31526fe
4mbk0r <4mbk0r@gmail.com>
2022-04-28 23:46:58 -0400

    correcciones222

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,7 @@ use App\Http\Controllers\PersonaCitaController;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return 'hola';
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
