7c213f21521b6683fef461852ff00e70eeca4482
4mbk0r <4mbk0r@gmail.com>
2022-05-03 11:29:17 -0400

    imprimir2

---
+++ storage/framework/views/f1375243e40dfd1ea79c02df158d340eb8386c46.php
@@ -0,0 +1,7 @@
+<h1> <?php echo e($name); ?></H1><br>
+<h1> <?php echo e($ap_paterno); ?></H1><br>
+<h1> <?php echo e($ap_materno); ?></H1><br>
+<h1> Fecha: <?php echo e($fecha); ?></H1><br>
+<h1> Hora: <?php echo e($hora); ?></H1><br>
+<h1> Sala: <?php echo e($sala); ?></H1><br>
+<?php /**PATH C:\xampp8.1\htdocs\example-app\resources\views/myPDF.blade.php ENDPATH**/ ?>
