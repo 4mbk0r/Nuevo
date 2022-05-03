babfb63a7e3fcb96b7faab3b314aac831ef206a7
4mbk0r <4mbk0r@gmail.com>
2022-05-03 11:36:32 -0400

    imprimire3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        
+        if(env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
