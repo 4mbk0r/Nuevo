b69480b0802028a452d638a77381b9035a44d8e4
4mbk0r <4mbk0r@gmail.com>
2022-05-24 11:57:52 -0400

    version de pruebadddd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
+        
         if(env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
-        }*/
+        }
     }
 }
 
